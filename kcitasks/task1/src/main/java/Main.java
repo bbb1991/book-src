310105e1084f8360fcf1ce15369000c24bf28b1e
Bagdat Bimaganbetov <b.bimaganbetov@innopolis.ru>
2018-03-02 01:11:02 +0300

    Completed task 1, step 3: Retrieve a connection from h2

--- kcitasks/task1/src/main/java/Main.java
+++ kcitasks/task1/src/main/java/Main.java
@@ -1,6 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-	System.out.println("Hello there!");
+	    System.out.println("Hello there!");
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:test");) {
+            System.out.println("Got connection!");
+            int major = connection.getMetaData().getDriverMajorVersion();
+            int minor = connection.getMetaData().getDriverMinorVersion();
+            System.out.printf("Driver version: %d.%d%n", major, minor);
+        } catch (SQLException e) {
+            System.err.println("Cannot get new connection!");
+            e.printStackTrace();
+        }
     }
 }
 
